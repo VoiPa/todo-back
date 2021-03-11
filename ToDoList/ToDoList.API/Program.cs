a87943e6af573c680ffec0cccc4770527bf4fe98
Pavel Voitkun <p.voitkun@yahoo.com>
2021-03-11 19:36:26 +0200

    [F7]SeedData
    Optimized Program class code

--- ToDoList/ToDoList.API/Program.cs
+++ ToDoList/ToDoList.API/Program.cs
@@ -15,22 +15,20 @@ namespace ToDoList.API
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
             {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationDbContext>();
-                    await context.Database.MigrateAsync();
-                    await Seed.SeedUsers(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occured during migration");
-                    
-                }
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedUsers(context);
             }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+
             await host.RunAsync();
         }
 
